fa038ee20f8c8494f345d931327b48ae0e44b24e
Florian Zaruba <fl.zaruba@be4web.net>
2015-06-06 13:03:26 +0200

    More figures

--- matlab/gaborFeatures.m
+++ matlab/gaborFeatures.m
@@ -51,15 +51,15 @@ img = double(img);
 
 
 %% Filtering
-figure('NumberTitle','Off','Name','Results');
+%figure('NumberTitle','Off','Name','Results');
 % Filter input image by each Gabor filter
 [u,v] = size(gaborArray);
 gaborResult = cell(u,v);
 for i = 1:u
     for j = 1:v
         gaborResult{i,j} = conv2(img,gaborArray{i,j},'same');
-        subplot(u,v,(i-1)*v+j);        
-        imshow(abs(gaborResult{i,j}),[]);
+        %subplot(u,v,(i-1)*v+j);        
+        %imshow(abs(gaborResult{i,j}),[]);
         % J{u,v} = filter2(G{u,v},I);
     end
 end
@@ -94,20 +94,20 @@ end
 %% Show filtered images
 
 % % Show real parts of Gabor-filtered images
-% figure('NumberTitle','Off','Name','Real parts of Gabor filters');
-% for i = 1:u
-%     for j = 1:v        
-%         subplot(u,v,(i-1)*v+j)    
-%         imshow(real(gaborResult{i,j}),[]);
-%     end
-% end
-% 
-% 
+ figure('NumberTitle','Off','Name','Real parts of Gabor filters');
+ for i = 1:u
+     for j = 1:v        
+         subplot(u,v,(i-1)*v+j)    
+         imshow(real(gaborResult{i,j}),[]);
+     end
+ end
+ 
+
 % % Show magnitudes of Gabor-filtered images
-% figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
-% for i = 1:u
-%     for j = 1:v        
-%         subplot(u,v,(i-1)*v+j)    
-%         imshow(abs(gaborResult{i,j}),[]);
-%     end
-% end
+ figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
+ for i = 1:u
+     for j = 1:v        
+         subplot(u,v,(i-1)*v+j)    
+         imshow(abs(gaborResult{i,j}),[]);
+     end
+ end
